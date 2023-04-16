f5faa3dfe6bc4cf9b1bba405a04fc834a5779bcc
Gabriel de Rezende Gonçalves <gabireze@gmail.com>
2023-04-15 22:21:59 -0300

    commenting out the code, changing the readme, changing the project name

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-# bitcoinfocusnow
+# Bitcoin Twitter Bot
+
+This is a simple Node.js application that posts the latest Bitcoin price, market cap, and volume information on Twitter using the [Twitter API v2](https://developer.twitter.com/en/docs/twitter-api).
+
+## Prerequisites
+
+Before running the application, you need to set up a Twitter developer account, create a new Twitter app, and obtain the following credentials:
+
+- **APP_KEY**: your Twitter app's API key
+- **APP_SECRET**: your Twitter app's API secret key
+- **ACCESS_TOKEN**: your Twitter account's access token
+- **ACCESS_SECRET**: your Twitter account's access token secret
+
+You also need to install Node.js and the following packages:
+
+- **axios**: a promise-based HTTP client for Node.js
+- **twitter-api-v2**: a wrapper library for the Twitter API v2
+- **dotenv**: a zero-dependency module that loads environment variables from a .env file
+
+## Installation
+
+1. Clone this repository to your local machine:
+
+```
+git clone https://github.com/your-username/bitcoin-twitter-bot.git
+```
+
+2. Install the required packages:
+
+```
+cd bitcoin-twitter-bot
+npm install
+```
+
+3. Create a **.env** file in the root directory of the project and add your Twitter API credentials:
+
+```
+APP_KEY=your-app-key
+APP_SECRET=your-app-secret
+ACCESS_TOKEN=your-access-token
+ACCESS_SECRET=your-access-token-secret
+```
+
+4. Run the application:
+
+```
+npm start
+```
+
+The application will fetch the latest Bitcoin price data from the [CoinGecko API](https://www.coingecko.com/api/documentations/v3), format the data into a tweet, and post it on Twitter using your account's credentials.
+
+## Customization
+
+You can customize the application by changing the following variables in the **index.mjs** file:
+
+- **CURRENCY**: the currency in which to display the price and market cap information (default: **"usd"**)
+- **COIN_ID**: the CoinGecko ID of the cryptocurrency to track (default: **"bitcoin"**)
+- **DAYS**: the number of days of historical data to retrieve from the CoinGecko API (default: **1**)
+  You can also modify the tweet message by editing the **getPriceData** function in the index.mjs file.
+
+## Follow us on Twitter
+
+Stay up to date with the latest #Bitcoin news and price updates by following [@BitcoinFocusNow](https://twitter.com/BitcoinFocusNow) on Twitter!
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](https://opensource.org/license/mit/) file for more information.
--- index.mjs
+++ index.mjs
@@ -1,13 +1,17 @@
-import axios from "axios";
-import { TwitterApi } from "twitter-api-v2";
-import * as dotenv from "dotenv";
+// Import required modules
+import axios from "axios"; // Used for making HTTP requests
+import { TwitterApi } from "twitter-api-v2"; // Used for posting tweets
+import * as dotenv from "dotenv"; // Used for loading environment variables
 
+// Set API URL, currency, and coin ID
 const COINGECKO_API_URL = "https://api.coingecko.com/api/v3";
 const CURRENCY = "usd";
 const COIN_ID = "bitcoin";
 
+// Load environment variables from .env file
 dotenv.config();
 
+// Initialize a new instance of Twitter API client using app key, app secret, access token, and access secret
 const twitterClient = new TwitterApi({
   appKey: process.env.APP_KEY,
   appSecret: process.env.APP_SECRET,
@@ -15,26 +19,36 @@ const twitterClient = new TwitterApi({
   accessSecret: process.env.ACCESS_SECRET,
 });
 
+// Async function to post a tweet
 async function postTweet() {
   try {
+    // Get price data from CoinGecko API
     const message = await getPriceData();
+
+    // Post tweet using Twitter API
     const tweet = await twitterClient.v2.tweet({ text: message });
+
+    // Log successful tweet post
     console.log(`Successfully posted tweet: ${JSON.stringify(tweet.data, null, 2)}`);
   } catch (error) {
+    // Log error if tweet post fails
     console.error(error);
   }
 }
 
+// Async function to get price data from CoinGecko API
 const getPriceData = async () => {
   try {
+    // Make GET request to CoinGecko API to get market chart data for the past day
     const response = await axios.get(`${COINGECKO_API_URL}/coins/${COIN_ID}/market_chart`, {
       params: {
         vs_currency: CURRENCY,
         days: 1,
       },
     });
-    const data = response.data;
 
+    // Extract relevant data from API response
+    const data = response.data;
     const currentPrice = data.prices.slice(-1)[0][1];
     const priceChange1h = (((currentPrice - data.prices[data.prices.length - 13][1]) / currentPrice) * 100).toFixed(2);
     const priceChange24h = (((currentPrice - data.prices[0][1]) / data.prices[0][1]) * 100).toFixed(2);
@@ -43,6 +57,7 @@ const getPriceData = async () => {
     const totalVolume = data.total_volumes.slice(-1)[0][1];
     const totalVolumeChange24h = (((totalVolume - data.total_volumes[0][1]) / data.total_volumes[0][1]) * 100).toFixed(2);
 
+    // Format message with relevant data
     const message1 = `🚀 #Bitcoin is currently trading at:\n💰 $${currentPrice.toLocaleString("en-US")} (${
       priceChange1h > 0 ? `+${priceChange1h}` : priceChange1h
     }% in the last 1 hour)\n\n`;
@@ -54,15 +69,23 @@ const getPriceData = async () => {
       totalVolumeChange24h > 0 ? `+${totalVolumeChange24h}` : totalVolumeChange24h
     }% in the last 24 hours)`;
 
+    // Concatenates all the messages and returns the final message to be posted on Twitter
     const final_message = message1 + message2 + message3 + message4;
 
     return final_message;
   } catch (error) {
+    // Catches any errors that may occur and logs the error to the console
     console.error(error);
   }
 };
 
+//Calls the postTweet() function to post the message on Twitter
 postTweet();
+
+/* This is a commented out function that exports the postTweet
+function for use in an AWS Lambda function. When uncommented,
+this function can be used to trigger the postTweet function on a
+regular schedule in the AWS environment. */
 
 // exports.handler = async function (event, context) {
 //   return postTweet();
